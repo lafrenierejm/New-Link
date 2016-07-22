51a99cab6cc8fb12ae94714e20453d7eab210af6
Joseph LaFreniere <joseph@lafreniere.xyz>
2016-07-22 12:27:52 -0500

    Add initial comment-based help to `New-Link()`
    Add a help section for synopsis, descrption, parameters, outputs, links,
    and components.

--- New-Link.psm1
+++ New-Link.psm1
@@ -1,4 +1,31 @@
-Function New-Link {
+function New-Link {
+    <#
+    .SYNOPSIS
+        Creates a filesystem link or shortcut.
+
+    .DESCRIPTION
+        The New-Link cmdlet provides a unified interface for creating any type of filesystem link, including shortcuts.
+
+    .PARAMETER Type
+        The type of link to create
+
+    .PARAMETER Source
+        The item which the created link will point to
+
+    .PARAMETER LinkPath
+        The path of the link to create
+
+    .OUTPUTS
+        Either a new WScript.Shell object if the specified type is 'Shortcut' or a new item of the specified type.
+
+    .LINK
+        New-Item
+        New-Object
+
+    .COMPONENT
+        New-Item
+        New-Object
+    #>
     [CmdletBinding(
         SupportsShouldProcess = $TRUE
     )]
@@ -16,17 +43,17 @@ Function New-Link {
     )
 
     process {
+        # PowerShell has no native shortcut support, so those are handled with COM objects
         if ($Type -eq 'Shortcut') {
             if ($PSCmdlet.ShouldProcess($LinkPath, 'Create Shortcut')) {
-# PowerShell has no native shortcut support, so use a COM object
                 $WshShell = New-Object -comObject WScript.Shell
-                    $Shortcut = $WshShell.CreateShortcut("$LinkPath")
-                    $Shortcut.TargetPath = "$Source"
-                    $Shortcut.Save()
+                $Shortcut = $WshShell.CreateShortcut("$LinkPath")
+                $Shortcut.TargetPath = "$Source"
+                $Shortcut.Save()
             }
         } else {
             if ($PSCmdlet.ShouldProcess($LinkPath, 'Create Link')) {
-# Starting with version 5.0, PoSH has native symlink support
+                # Starting with version 5.0, PoSH has native symlink support
                 New-Item -Path "$LinkPath" -ItemType $Type -Value "$Source"
             }
         }
